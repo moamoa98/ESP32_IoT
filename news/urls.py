d6e59522ea79351eb24c5bec49fce2724b8e39f3
Thái Quang Hiền <55608506+moamoa98@users.noreply.github.com>
2024-12-19 23:13:47 +0700

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -8,7 +8,7 @@ from .views import index,led_control,get_temp_humidity,get_historical_data
 urlpatterns = [
     path('', index, name='index'),
     # path('led_control/',led_control, name='led_control'),
-    # path('get_temp_humidity/',get_temp_humidity, name='get_temp_humidity'),
+    path('get_temp_humidity/',get_temp_humidity, name='get_temp_humidity'),
     path('api/temp-humidity/', get_temp_humidity),
     path('api/historical-data/', get_historical_data),
     path('api/device/control/', led_control),
