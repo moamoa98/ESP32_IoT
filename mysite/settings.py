5db27dd18d1d1ca5058262de9342719d43c826b0
Thái Quang Hiền <55608506+moamoa98@users.noreply.github.com>
2024-12-28 00:40:45 +0700

    up

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,23 +92,27 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.postgresql',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         # 'NAME': 'railway',
+#         # 'USER':'postgres',
+#         # 'USER':'root',
+#         # 'PASSWORD':'gpEmgqzZYBowuvDqpvcPlNTAhagwEFvz',
+#         # 'PASSWORD':'',
+#         # 'HOST':'junction.proxy.rlwy.net',
+#         # 'PORT':'55524',
+      
+#     }
+       
+# }
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': 'railway',
-        # 'USER':'postgres',
-        # 'USER':'root',
-        # 'PASSWORD':'gpEmgqzZYBowuvDqpvcPlNTAhagwEFvz',
-        # 'PASSWORD':'',
-        # 'HOST':'junction.proxy.rlwy.net',
-        # 'PORT':'55524',
-      
+        'ENGINE': 'django.db.backends.dummy'
     }
-       
 }
-
 
 
 # Password validation
